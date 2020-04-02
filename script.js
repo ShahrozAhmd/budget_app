c22f0c566f72089b597bc1a23688bee7661a3f6b
Shahroz Ahmed <shahrozahmed286@gmail.com>
2020-04-02 21:52:20 +0500

    add event for keypress and click

--- script.js
+++ script.js
@@ -1,30 +1,35 @@
-
 //control the budget
-var budgetController = (function(){
-
-})();
+var budgetController = (function() {})();
 
 
 
 
 //control the UI
-var UIController = (function(){
+var UIController = (function() {})();
 
-})();
+
 
 
 
 //trigger all actions
-var trigger = (function(budgetCtrl, UICtrl){
-    
+var trigger = (function(budgetCtrl, UICtrl) {
+
+  var triggerCtrl = function() {
+      
     // 1. Get the data from the input field.
-    
-   // 2. Add data to the budget controller.
-   
-   // 3. Add new item to the UI.
+    // 2. Add data to the budget controller.
+    // 3. Add new item to the UI.
+    // 4. Calculate the budget.
+    // 5. Update the budget on UI
 
-   // 4. Calculate the budget.
+    console.log('Button Works');
+  }
+  
+  document.querySelector(".add-btn").addEventListener("click",triggerCtrl);
+  document.addEventListener('keypress',function(e){
+      if(e.keyCode === 13 || e.which === 13){
+          triggerCtrl();
+      }
+  })
 
-   // 5. Update the budget on UI.
-
-})(budgetController,UIController);
+})(budgetController, UIController);
