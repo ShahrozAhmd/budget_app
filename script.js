e73618d472e64a5b14150f52109d6664c6aef241
Shahroz Ahmed <shahrozahmed286@gmail.com>
2020-04-02 21:40:44 +0500

    added modules

---
+++ script.js
@@ -0,0 +1,30 @@
+
+//control the budget
+var budgetController = (function(){
+
+})();
+
+
+
+
+//control the UI
+var UIController = (function(){
+
+})();
+
+
+
+//trigger all actions
+var trigger = (function(budgetCtrl, UICtrl){
+    
+    // 1. Get the data from the input field.
+    
+   // 2. Add data to the budget controller.
+   
+   // 3. Add new item to the UI.
+
+   // 4. Calculate the budget.
+
+   // 5. Update the budget on UI.
+
+})(budgetController,UIController);
