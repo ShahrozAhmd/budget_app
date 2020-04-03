5907936f8ac08487626b6c15b37ed70dd6ab27bc
Shahroz Ahmed <shahrozahmed286@gmail.com>
2020-04-03 16:45:10 +0500

    created constructors and data structor in budget controller module to hold and calculate data

--- script.js
+++ script.js
@@ -1,35 +1,101 @@
 //control the budget
-var budgetController = (function() {})();
+var budgetController = (function () {
 
+  //make constructors to create instances of 
+  //all income and expense as objects
+
+  function Expenses(id, desc, val) {
+    this.id = id;
+    this.desc = desc;
+    this.val = val;
+  }
+
+
+  function Incomes(id, desc, val) {
+    this.id = id;
+    this.desc = desc;
+    this.val = val;
+  }
+
+  //create a data structure to  hold all data 
+
+  var data = {
+    allIncExpe: {
+      inc: [],
+      exp: []
+    },
+    totals: {
+      totalIncome: 0,
+      totalExpense: 0
+    }
+
+  };
+
+
+})();
 
 
 
 //control the UI
-var UIController = (function() {})();
+var UIController = (function () {
+  var getDataClasses = {
+    type: ".select-box",
+    description: ".des-box",
+    value: ".value-box",
+    addBtn: ".add-btn"
+  };
 
-
-
-
+  return {
+    //getting data of all fields in object properties
+    inputData: function () {
+      return {
+        type: document.querySelector(getDataClasses.type).value,
+        description: document.querySelector(getDataClasses.description).value,
+        value: document.querySelector(getDataClasses.value).value
+      };
+    },
+    dataClasses: function () {
+      return getDataClasses;
+    }
+  };
+})();
 
 //trigger all actions
-var trigger = (function(budgetCtrl, UICtrl) {
+var trigger = (function (budgetCtrl, UICtrl) {
 
-  var triggerCtrl = function() {
-      
+  var eventHandler = function () {
+    console.log('Appication has started');
+    //store the passed data of class names of UI elements
+    var dataCl = UICtrl.dataClasses();
+
+    document.querySelector(dataCl.addBtn)
+      .addEventListener("click", triggerCtrl);
+
+    document.addEventListener("keypress", function (e) {
+      if (e.keyCode === 13 || e.which === 13) {
+        triggerCtrl();
+      }
+    });
+  };
+
+
+
+  var triggerCtrl = function () {
+
     // 1. Get the data from the input field.
+    var inputValues = UICtrl.inputData();
     // 2. Add data to the budget controller.
     // 3. Add new item to the UI.
     // 4. Calculate the budget.
     // 5. Update the budget on UI
+  };
 
-    console.log('Button Works');
+  return {
+    init: function () {
+      eventHandler();
+    }
   }
-  
-  document.querySelector(".add-btn").addEventListener("click",triggerCtrl);
-  document.addEventListener('keypress',function(e){
-      if(e.keyCode === 13 || e.which === 13){
-          triggerCtrl();
-      }
-  })
 
 })(budgetController, UIController);
+
+trigger.init();
